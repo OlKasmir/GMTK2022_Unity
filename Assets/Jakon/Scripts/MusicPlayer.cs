64f20f4bbfaf10e0feb1164e06e3e9d580d9eb9a
IJakon <109367252+IJakon@users.noreply.github.com>
2022-07-16 16:28:38 +0200

    Options Logik mit Volume Slider hinzugefügt

---
+++ Assets/Jakon/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource[] AudioSource;
+    private float musicVolume = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (AudioSource source in AudioSource)
+        {
+            source.volume = musicVolume;
+        }
+    }
+
+    public void UpdateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
--- Assets/Jakon/Scripts/UiController.cs
+++ Assets/Jakon/Scripts/UiController.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UiController : MonoBehaviour
 {
+    public GameObject optionMenu;
+    public GameObject mainMenu;
+    public GameObject canvas;
     // Start is called before the first frame update
+    private void Start()
+    {
+        mainMenu = GameObject.Find("Canvas/Main Menu");
+        optionMenu = GameObject.Find("Canvas/OptionsMenu");
+        optionMenu.SetActive(false);
+    }
     public void StartGame()
     {
         Debug.Log("Input First Game Scene here");
@@ -17,12 +27,20 @@ public class UiController : MonoBehaviour
 
     public void Options()
     {
-        Debug.Log("Input Option Scene here");
+ 
+        optionMenu.SetActive(true);
+        mainMenu.SetActive(false);
     }
     public void End()
     {
         //Application.Quit();
         UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void Back()
+    {
+        optionMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
 
